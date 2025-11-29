dffd1929ea40ce69fda6392acfa650c4dde57db9
Mathesh-299 <postbox2992005@gmail.com>
2025-11-29 08:48:58 +0530

    Adding server.js

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config()
+const DB = require("./config/DB");
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 8000
+DB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    })
+    .catch(() => {
+        console.log("Failed to connect to DB, server not started");
+    })
+
+app.get("/api/", (req, res) => {
+    res.send(`<h1>API is working</h1>`)
+})
+
+app.use("/api/auth", require("./router/userAuthRouter"))
